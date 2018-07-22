7da538ee3d2d40e9c4b5bc644052f468b6da71b8
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-07-22 18:27:51 -0400

    add mock data in the friends javascript file

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Jaspreet Kaur",
+        photo:"http://www.status77.in/wp-content/uploads/2015/08/Awesome-Profile-Pictures-2015..jpg",
+        scores: [
+            "5",
+            "4",
+            "2",
+            "1",
+            "3",
+            "1",
+            "5",
+            "3",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Taranjit Singh",
+        photo:"https://www.windowscentral.com/sites/wpcentral.com/files/styles/xlarge_wm_brw/public/field/image/2016/09/photo-examine-monitor-derek-hero.jpg?itok=8tJZIzzW",
+        scores: [
+            "2",
+            "2",
+            "4",
+            "2",
+            "5",
+            "1",
+            "4",
+            "2",
+            "3",
+            "5"
+        ]
+    }
+
+];
+
+module.exports = friends;

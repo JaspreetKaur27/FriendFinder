3dcf536fd7bb68f4337f120f238c521e071a30e8
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-07-22 18:30:12 -0400

    add api routes to get and post dat in the json form

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friends = require("../data/friends.js");
+
+module.exports = (app) => {
+
+    app.get("/api/friends", (req, res) => {
+        res.json(friends);
+    });
+
+    app.post('/api/friends', (req, res) => {
+        friends.push(req.body);
+    });
+}
